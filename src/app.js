e81b4fb279d4f3ab0e003219ee3a67527568eaf2
MarianaBMoraes <marianabmoraes.ef@gmail.com>
2024-10-14 19:04:04 -0300

    feat

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import e from "express";
 import workshop_router from "./routers/workshopRouter.js";
 import vehicle_router from "./routers/vehicleRouter.js";
 import maintenance_router from "./routers/maintenanceRouter.js";
-// import maintenance_router "./routers/maintenanceRouter.js";
 
 const app = e();
 
@@ -13,4 +12,4 @@ app.use("/workshop", workshop_router);
 app.use("/vehicle", vehicle_router);
 app.use("/maintenance", maintenance_router);
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(3000, () => console.log("Server is running on port 3000"));
